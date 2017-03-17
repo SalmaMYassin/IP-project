798a1d2bcea30e88ce5d73c36674ee4a4a1481a2
SalmaMYassin <salmamyassinn@gmail.com>
2017-03-17 10:57:05 +0200

    Favicon Added

---
+++ IP Project/JS.js
@@ -0,0 +1,61 @@
+/**
+ * Created by Omar on 3/6/2017.
+ */
+
+
+/*Slider*/
+var slideIndex = 0;
+window.addEventListener("load",showSlides,true);
+
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+
+//----------------------------------------------------------------------------------------------------------------//
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+
+$(document).ready(function() {
+    $('#myBtn').click(function () {
+        $("html, body").animate({scrollTop: 0}, 1700);
+        return false;
+    });
+
+});
+
+/*---------------------------------------------  dropdown  -------------------------------------------*/
+
+
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+        var myDropdown = document.getElementById("myDropdown");
+        if (myDropdown.classList.contains('show')) {
+            myDropdown.classList.remove('show');
+        }
+    }
+}
